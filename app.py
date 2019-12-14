11005edb698ea2e90bf8243505af270a7864c330
Nishon Tandukar <nishon.tan@gmail.com>
2019-12-14 14:36:41 +0545

    Adds demo message

--- app.py
+++ app.py
@@ -7,12 +7,14 @@ data = {'message':'The air quality is great'}
 data2 = {'message':'The air quality is imporving (reply from flask)'}
 
 @app.route('/')
-def fetch_aqi():
-    return json.dumps(data2)
-
-@app.route('/aqi', methods=['POST'])
 def hello():
     return json.dumps(data)
+
+@app.route('/aqi', methods=['POST'])
+def fetch_aqi():
+    data = request.get_json(silent=True)
+    print(data) 
+    return json.dumps(data2)
 
 
 if __name__ == '__main__':
